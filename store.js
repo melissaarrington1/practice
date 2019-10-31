65ac69fe7f4554e5b4a85e452b6e43df3455c16b
Melissa Arrington <melissa.arrington1@yahoo.com>
2019-10-30 23:29:27 -0400

    updated js. added functionality for shopping cart total

--- store.js
+++ store.js
@@ -7,5 +7,23 @@ for (let i = 0; i < removeCartItemButtons.length; i++) {
   button.addEventListener("click", function(event) {
     var buttonClicked = event.target;
     buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
   });
+}
+
+function updateCartTotal() {
+  let cartItemContainer = document.getElementsByClassName("all-cart-items")[0];
+  let cartRows = cartItemContainer.getElementsByClassName("cart-item");
+  let total = 0;
+  for (i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i];
+    var priceElement = cartRow.getElementsByClassName("cart-item-price")[0];
+    var quantityElement = cartRow.getElementsByClassName("cart-quantity")[0];
+
+    let price = parseFloat(priceElement.innerHTML.replace("$", ""));
+    let quantity = quantityElement.value;
+    console.log(price * quantity);
+    total = total + price * quantity;
+  }
+  document.getElementsByClassName("cart-total")[0].innerHTML = total;
 }
